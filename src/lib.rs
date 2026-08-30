5e7918920a2919f8a7776cdf1d0868ab4c418bc3
0xans <151745138+0xans@users.noreply.github.com>
2026-08-30 17:00:07 +0300

    Change module declarations to public in lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,12 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
-mod ssn;
-mod hashes;
-mod resolver;
-mod types;
-mod invoke;
-mod macros;
+pub mod ssn;
+pub mod hashes;
+pub mod resolver;
+pub mod types;
+pub mod invoke;
+pub mod macros;
+pub mod nt;
 
 #[cfg(feature = "std")]
 extern crate std;
@@ -14,16 +15,16 @@ use core::ffi::c_void;
 /**
  * Initialize the syscall engine
  * */
-pub unsafe fn initialize() -> bool {
+pub unsafe fn initialize() -> bool { unsafe {
     ssn::initialize(core::ptr::null_mut())
-}
+}}
 
 /**
  * Get a reference to the global syscall table
  * */
-pub unsafe fn syscall_table() -> &'static ssn::SyscallTable {
+pub unsafe fn syscall_table() -> &'static ssn::SyscallTable { unsafe {
     ssn::syscall_table()
-}
+}}
 
 /**
  * Compute the DJB2 hash of a function name (ASCII, case insensitive)
@@ -42,21 +43,21 @@ pub const fn hash_module_name_wide(name: &[u16]) -> u32 {
 /**
  * Resolve a loaded module by its wide name hash
  * */
-pub unsafe fn resolve_module(hash: u32) -> *mut c_void {
+pub unsafe fn resolve_module(hash: u32) -> *mut c_void { unsafe {
     resolver::ldr_module_search(hash)
-}
+}}
 
 /**
  * Resolve a single export from a module by tis function name hash
  * */
-pub unsafe fn resolve_function(module: *mut c_void, hash: u32) -> *mut c_void {
+pub unsafe fn resolve_function(module: *mut c_void, hash: u32) -> *mut c_void { unsafe {
     resolver::ldr_function_by_hash(module, hash)
-}
+}}
 
 
 /**
  * Resolve an additional syscall by hash after initialization
  * */
-unsafe fn resolve_extra_ssn(hash: u32) -> bool {
+unsafe fn resolve_extra_ssn(hash: u32) -> bool { unsafe {
     ssn::resolve_ssn(hash)
-}
+}}
